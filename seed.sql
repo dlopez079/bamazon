5f2310867132d5f34b89f7ec99187a9fcea6726b
dlopez079 <dlopez079@gmail.com>
2019-10-03 14:58:29 -0400

    Changed schema file from bamazon_db.sql to schema.sql.  Created a new file called seed.sql.  Revised the readproducts function

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Buzz Light Year", "Toy", 2.50, 100);
+
+
+-- ### Alternative way to insert more than one row
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
