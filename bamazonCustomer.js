fb3ddc583b1b1dc6f5470dd37bef56a0ada36575
dlopez079 <dlopez079@gmail.com>
2019-10-03 10:14:16 -0400

    Added the readProducts() function so we can read data from the database.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,7 +20,16 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    createProduct();
+    readProducts();
   });
 
 //Display all the items available for sale.  
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
