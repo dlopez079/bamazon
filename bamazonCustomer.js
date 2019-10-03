3d2c8426f248ab0dc47a7c3a66ac7763ebb2a4d8
dlopez079 <dlopez079@gmail.com>
2019-10-03 10:11:54 -0400

    Listed the external required scripts (inquirer and mysql) for this application.  Installed mysql npm package.  Created connection from bamazoncustomer.js to mysql database.

---
+++ bamazonCustomer.js
@@ -0,0 +1,26 @@
+//listed the externale required scripts for this application.
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+//CREATED CONNECTION TO SQL DATABASE
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "ice_creamDB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    createProduct();
+  });
+
+//Display all the items available for sale.  
