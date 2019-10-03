a47e3f70f66f99e5950b7cbb878b4db7064bb398
dlopez079 <dlopez079@gmail.com>
2019-10-03 09:51:18 -0400

    Successfully created bamazon database script.  Executed the script successfully on DBeaver.  Database is successfully created.

--- bamazon.sql
+++ bamazon.sql
@@ -5,21 +5,17 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  flavor VARCHAR(45) NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255) NULL,
+  department_name VARCHAR(255) NULL,
   price DECIMAL(10,2) NULL,
-  quantity INT NULL,
-  PRIMARY KEY (id)
+  stock_quantity INT NULL,
+  PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Buzz Light Year", "Toy", 2.50, 100);
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
 
 -- ### Alternative way to insert more than one row
 -- INSERT INTO products (flavor, price, quantity)
